f72a6300d267d23da76a4917956fb56bf321643c
pane2004 <alexlu2000clash@gmail.com>
2024-09-29 02:31:43 -0400

    update scraper with IMAGE

--- backend/db/scrape.py
+++ backend/db/scrape.py
@@ -10,38 +10,42 @@ import os
 
 load_dotenv()
 
-os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
 
 class Features(BaseModel):
-  key: str
-  value: str
+    key: str
+    value: str
+
 
 class ClothingItem(BaseModel):
-  imgUrl: str
-  shoppingUrl: str
-  type: str
-  price: str
-  color: str
-  brand: str
-  style: str
-  material: str
-  features: List[Features]
-  additionalClothingProperties: List[str]
+    imgUrl: str
+    shoppingUrl: str
+    type: str
+    price: str
+    color: str
+    brand: str
+    style: str
+    material: str
+    features: List[Features]
+    additionalClothingProperties: List[str]
+
 
 params = {
-  "api_key": os.getenv("SCRAPER_API_KEY"),
-  "engine": "google_shopping",
-  "google_domain": "google.com",
-  "q": "nike sneakers",
-  "hl": "en",
-  "gl": "us",
-  "location": "United States",
-  "direct_link": "true"
+    "api_key": os.getenv("SCRAPER_API_KEY"),
+    "engine": "google_shopping",
+    "google_domain": "google.com",
+    "q": "nike sneakers",
+    "hl": "en",
+    "gl": "us",
+    "location": "United States",
+    "direct_link": "true",
 }
 
 vectors_arr = []
 json_arr = []
 clothing_items = []
+
 
 def create_data_json_dump():
     """
@@ -52,15 +56,18 @@ def create_data_json_dump():
 
     client = OpenAI()
     for product in results.get("shopping_results", []):
-      response = client.beta.chat.completions.parse(
-      model="gpt-4o-mini",
-      messages=[
-          {
-              "role": "user",
-              "content": f'''
+        response = client.beta.chat.completions.parse(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": f"""
               JSON:
               {{ type: "Shoes" | "Jacket" | "Shirt" | "Pants" | "Dress" | "Hat" | "Glasses" | "Chain" | "Sweater" | "Skirt", 
-              color: "string", brand: "string", "style": "string", material: "string", features: {{}}, 
+              color: "string", brand: "string", "style": "string", material: "string", pattern: "string", features: {{}}, 
               additionalClothingProperties: [] }} 
 
               EXTRA_DATA:
@@ -69,22 +76,31 @@ def create_data_json_dump():
               Prompt:
               Based on the attached clothing image and EXTRA_DATA from the google product page, fill in the attached JSON file 
               along with any additional properties that can be observed on the clothing. Ignore sizing and condition details.
-              '''
-          },
-      ],
-      response_format=ClothingItem,
-      )
-      response_product = response.choices[0].message.parsed
-      response_product.imgUrl = product["thumbnail"]
-      response_product.shoppingUrl = product["link"]
-      response_product.price = product["price"]
-      clothing_items.append(response_product)
+              """,
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": product['thumbnail'],
+                            },
+                        },
+                    ],
+                },
+            ],
+            response_format=ClothingItem,
+        )
+        response_product = response.choices[0].message.parsed
+        response_product.imgUrl = product["thumbnail"]
+        response_product.shoppingUrl = product["link"]
+        response_product.price = product["price"]
+        clothing_items.append(response_product)
 
-      # this is the json of the product
-      json_result = response_product.dict()
-      print(json_result)
-      json_arr.append(json_result)
-        
+        # this is the json of the product
+        json_result = response_product.dict()
+        print(json_result)
+        json_arr.append(json_result)
+
     return json_arr
+
 
 generate_data(create_data_json_dump())
