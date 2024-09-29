850d55c3b67de5f1e1d57a865963010a16e007a2
Josh <joshual2004.work@gmail.com>
2024-09-29 02:15:12 -0400

    updated sam to 2 endpoints

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from sam.sam import run_sam
 
 app = Flask(__name__)
 
-
 @app.route('/sam', methods = ['POST'])
 def sam():
     x_coord = request.args.get('x_coord')
@@ -12,3 +11,8 @@ def sam():
     file.save('static/input.jpg')
     run_sam(x_coord, y_coord)
     return send_file('static/mask.jpg', mimetype='image/jpeg')
+
+
+@app.route('/get_bitmask', methods = ['GET'])
+def get_bitmask():
+    return send_file('static/bitmask.jpg', mimetype='image/jpeg')
--- backend/sam/sam.py
+++ backend/sam/sam.py
@@ -1,6 +1,4 @@
-from segment_anything import SamPredictor, sam_model_registry
-import numpy as np
-import matplotlib.pyplot as plt
+from ultralytics import SAM
 import cv2
 import time
 import warnings
@@ -9,26 +7,18 @@ warnings.filterwarnings("ignore")
 def run_sam(x_coord, y_coord):
     start_time = time.time()
     
-    image = cv2.imread('static/input.jpg')
+    source = 'static/input.jpg'
+    image = cv2.imread(source)
 
-    sam_checkpoint = "sam/sam_vit_l_0b3195.pth"
-    model_type = "vit_l"
-    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-    predictor = SamPredictor(sam)
+    model = SAM("sam/mobile_sam.pt")
+    results = model(source, points=[[int(x_coord), int(y_coord)]], labels=[1])
+    mask = results[0].masks.data.detach().numpy().squeeze()
+    print("--- %s seconds ---" % (time.time() - start_time))
 
-    predictor.set_image(image)
-    input_point = np.array([[x_coord, y_coord]])
-    input_label = np.array([1])
-    masks, scores, logits = predictor.predict(
-        point_coords=input_point,
-        point_labels=input_label,
-        multimask_output=False,
-    )
-    mask = masks[0]
-    plt.figure(figsize=(10,10))
-    plt.imshow(image)
+    print(mask)
+    cv2.imwrite("static/bitmask.jpg", mask.astype(int) * 255)
+
     image[mask < 0.5] = [255, 255, 255]
-    plt.gca().imshow(image)
     cv2.imwrite("static/mask.jpg", image)
 
     print("--- %s seconds ---" % (time.time() - start_time))
