8137df208e76b3e2039a23e1bc217dce3bdb5f5c
Gavin Song <gavins3math@gmail.com>
2024-09-29 10:20:45 -0400

    update

--- backend/db/scrape.py
+++ backend/db/scrape.py
@@ -35,7 +35,7 @@ params = {
   "api_key": os.getenv("SCRAPER_API_KEY"),
   "engine": "google_shopping",
   "google_domain": "google.com",
-  "q": "mens jeans",
+  "q": "mens denim jacket",
   "hl": "en",
   "gl": "us",
   "location": "United States",
